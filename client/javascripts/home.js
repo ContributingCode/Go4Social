9cd5ef2b7a4a15bceff9bea1baf7970f64e1cc7b
Jinchao Ye <yjc111@gmail.com>
2012-07-27 02:51:46 -0700

    new Feature: Map. UI revised.

--- client/javascripts/home.js
+++ client/javascripts/home.js
@@ -1,8 +1,6 @@
 $("#page-home").bind('pageinit', function() {
         
-    $("#user_name").text("Jinchao Ye");
-    $("#user_image").attr("src","http://profile.ak.fbcdn.net/hprofile-ak-ash2/372183_100002081796756_1036753949_q.jpg");
-    $("#user_location").text("Stanford, California");
+
     //fb authorization
     // Load the SDK Asynchronously
     (function(d){
---
+++ client/javascripts/profile.js
@@ -0,0 +1,7 @@
+$("#page-home").bind('pageinit', function() {
+
+$("#user_name").text("Jinchao Ye");
+$("#user_image").attr("src","http://profile.ak.fbcdn.net/hprofile-ak-ash2/372183_100002081796756_1036753949_q.jpg");
+$("#user_location").text("Stanford, California");
+
+});
